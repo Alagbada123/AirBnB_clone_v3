a479ab0d3f5ed1742305b4f26de1e2770f0b945b
Alagbada123 <114651605+Alagbada123@users.noreply.github.com>
2025-02-14 00:35:24 +0100

    3-deploy_web_static.py

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -8,7 +8,7 @@ from fabric.operations import run, put, sudo, local
 from datetime import datetime
 import os
 
-env.hosts = ['66.70.184.249', '54.210.138.75']
+env.hosts = ['3.90.65.231', '100.26.212.56']
 created_path = None
 
 
